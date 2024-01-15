7a65d20c43a7308e7417dc6ff6975c58adf8c034
JosueDev22 <77304305+Josuba22@users.noreply.github.com>
2024-01-15 20:30:17 -0300

    aulas css e js

---
+++ Módulo 02/06 - Codificação para Front-End/3 - JavaScript/Aula 02/Atividade 15-01-24/atividade-15-01-24.js
@@ -0,0 +1,66 @@
+/*1) Use o comando alert para exibir o resultado da expressão 3+5.*/
+
+//alert (3+5);
+
+/*2)Crie uma variável chamada "nome" com o valor "Alice" e uma variável chamada "idade" com o valor 30. Use o comando alert para exibir uma 
+mensagem concatenando o nome e a idade, como "Alice tem 30 anos".*/
+
+/*let nome = "Alice";
+let idade = 30;
+
+alert (nome + " tem " + idade + " anos.")*/
+
+/*3) Crie uma variável "preço" com o valor 24.99 e uma variável "quantidade" com o valor 5. Use o comando alert para exibir o resultado do
+cálculo do preço total, ou seja, preço x quantidade.*/
+
+/*let preço = 24.99
+let quantidade = 5
+
+alert (preço * quantidade);*/
+
+/*4) Escreva uma expressão que verifica se o número 7 é maior do que 3 e use o comando "alert" para exibir o resultado como um valor booleano.*/
+
+/*if (7 > 3) {
+    alert ("verdadeiro!")
+}
+
+else {
+    alert("Falso")
+}*/
+
+/*5) Crie uma variável "nome", "idade" e "altura". Peça ao usuário para informar cada uma dessas informações, por fim, monte uma única
+mensagem usando o comando alert contendo todas as informações.*/
+
+/*
+método 1:
+
+let nome = prompt("Digite seu nome")
+let idade = prompt("Digite sua idade")
+let altura = prompt("Digite sua altura")
+
+alert(nome + " tem " + idade + " anos e " + altura + " metros de altura.");*/
+
+/*método 2:
+
+let nome = prompt("Digite seu nome:");
+let idade = prompt("Digite sua idade:");
+let altura = prompt("Digite sua altura:");
+
+alert("Nome: " + nome + "\nIdade: " + idade + "\nAltura: " + altura);*/
+
+/*6) Faça um programa onde o usuário informa um valor monetário na moeda Real Brasileira (R$) e converta esse valor para Dolar Americano (U$),
+levando em consideração que U$ 1 Dólar custa R$ 4,90 Reais. Por fim, mostre o valor convertido.*/
+
+/*let real = prompt("Digite o valor em Reais")
+let cotacao = 4.90
+let valorDolar = real / cotacao
+
+alert("O valor em Dólar é: " + valorDolar.toFixed(2));*/
+
+/*7) Peça ao usuário para informar um nome e depois mostre esse nome em um elemento H1 em uma página HTML.*/
+
+let nome = prompt("Digite seu nome")
+
+document.getElementById("titulo") = nome
+
+/*8) No HTML crie um paragráfo da cor AZUL e no JavaScript altere essa cor para VERMELHO.*/
---
+++ Módulo 02/06 - Codificação para Front-End/3 - JavaScript/Aula 02/exemplo 01/exemplo-01.js
@@ -0,0 +1,7 @@
+function ligar () {
+    document.getElementById('luz').src = "pic_bulbon.gif"
+}
+
+function desligar () {
+    document.getElementById('luz').src = "pic_bulboff.gif"
+}
