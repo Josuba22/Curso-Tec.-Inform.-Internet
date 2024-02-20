43ed3b03da5dba6301153e3cafdd2dba255ab309
Josuba22 <businessjop22@gmail.com>
2024-02-20 11:30:10 -0300

    Aula 20.02.24
    Aula sobre API

--- Módulo 02/07 - Interação com API/Aula 05/exemplo-10.js
+++ Módulo 02/07 - Interação com API/Aula 05/exemplo-10.js
@@ -3,11 +3,36 @@ async function busca(){
     //o fetch faz uma BUSCA no json
     let procura = await fetch("lista-produtos.json")
     let produtos = await procura.json()
-    console.log(produtos)
+    //console.log(produtos)
 
     let listaDiv = document.getElementById("lista-card")
-    for(let x in produtos){
-        listaDiv.innerHTML += `<h1> ${produtos[x].nome} </h1>`
+
+    //o of no for pega o conteudo do objeto
+    for(let produto of produtos){
+        listaDiv.innerHTML += `
+            <div class="card">
+                <a href="#">
+                    <img
+                        src="${produto.img}" 
+                        width="250" 
+                        height="auto"
+                    >
+                </a>
+                <h3> ${produto.nome} </h3>
+                <p> ${produto.descricao} </p>
+                <br>
+                <div>
+                    <span class="semDesconto">
+                            R$ ${(produto.valorSemDesconto).toFixed(2).replace(".",",")}
+                    </span>
+                    <span class="comDesconto">
+                        <b>
+                            R$ ${(produto.valorComDesconto).toFixed(2).replace(".",",")}
+                        </b>
+                    </span>
+                </div>
+            </div>
+        `
     }
 
 }
