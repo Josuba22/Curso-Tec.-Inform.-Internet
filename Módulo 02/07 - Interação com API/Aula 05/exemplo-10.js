b8879c638267133468d1bb03a4e5580f64b799ce
Josuba22 <businessjop22@gmail.com>
2024-02-21 11:21:38 -0300

    aula dia 21.02.24
    Criação de site e-commerce

--- Módulo 02/07 - Interação com API/Aula 05/exemplo-10.js
+++ Módulo 02/07 - Interação com API/Aula 05/exemplo-10.js
@@ -11,25 +11,28 @@ async function busca(){
     for(let produto of produtos){
         listaDiv.innerHTML += `
             <div class="card">
-                <a href="#">
-                    <img
-                        src="${produto.img}" 
-                        width="250" 
-                        height="auto"
-                    >
-                </a>
-                <h3> ${produto.nome} </h3>
-                <p> ${produto.descricao} </p>
-                <br>
-                <div>
-                    <span class="semDesconto">
-                            R$ ${(produto.valorSemDesconto).toFixed(2).replace(".",",")}
-                    </span>
-                    <span class="comDesconto">
-                        <b>
-                            R$ ${(produto.valorComDesconto).toFixed(2).replace(".",",")}
-                        </b>
-                    </span>
+                <div class="grupo-img">
+                    <a href="produtos-10.html">
+                        <img
+                            src="${produto.img}" 
+                            width="auto" 
+                            height="auto"
+                        >
+                    </a>
+                </div>
+
+                <div class="textos">
+                    <h3> ${produto.nome} </h3>
+                    <p> ${produto.descricaoBreve} </p>
+                    <div>
+                        <span class="semDesconto">
+                                R$ ${(produto.valorSemDesconto).toFixed(2).replace(".",",")} <!-- O comando replace() serve para substituir um simbolo ou string por outra, no caso do exemplo, o ponto (.) pela vírgula (,). -->
+                        </span>
+                        <br>
+                        <span class="comDesconto">
+                                R$ ${(produto.valorComDesconto).toFixed(2).replace(".",",")} <!-- O comando toFixed(2) serve para definir a quantidade de casas decimais após a vírgula. -->
+                        </span>
+                    </div>
                 </div>
             </div>
         `
